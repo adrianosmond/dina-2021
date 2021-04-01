72491ef2a6caf05b5c81e9e99f26377abfffaa1d
adrianosmond <adrian.osmond@ev-box.com>
2021-04-01 19:04:46 +0200

    Initial commit with CRA, Tailwind, Prettier and ESLint

---
+++ README.md
@@ -0,0 +1,5 @@
+# Dina 2021
+
+A project I made for Dina's birthday in 2021
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ src/App.js
@@ -0,0 +1,9 @@
+const App = () => (
+  <div className="p-8">
+    <h1 className="font-bold text-2xl">
+      Create React App, Tailwind, Prettier, ESLint
+    </h1>
+  </div>
+);
+
+export default App;
